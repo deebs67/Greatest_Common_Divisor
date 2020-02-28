3bc1bb77ae2cf51da79c0c07efda52bb12ebdf34
ddnb <damian.bevan@aceaxis.co.uk>
2020-02-28 20:52:15 +0000

    First push of code - no algorithm coded yet, only boilerplate

---
+++ Greatest_Common_Divisor/Greatest_Common_Divisor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greatest_Common_Divisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entry
+            Console.Title = "Greatest Common Divisor";
+
+            //////////////////////////////////////////////////////////////////
+            // Main code
+            //////////////////////////////////////////////////////////////////
+
+            // Get first number
+            Console.WriteLine("Enter first integer:");
+            long first_integer = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"The first integer you chose was: {first_integer}\n");
+
+            // Get second number
+            Console.WriteLine("Enter second integer:");
+            long second_integer = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"The second integer you chose was: {second_integer}\n");
+
+            // TODO:
+            // Write recursive function to carry out a step of the algorithm, as described here:
+            // https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+            // Exit
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
